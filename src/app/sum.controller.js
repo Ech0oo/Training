5ceb7b01f25af8ac281bebbc1ae3e991503ada51
Ech0oo <Ech0oo@tut.by>
2016-02-10 19:29:14 +0300

    Lesson 2: extra task draft

--- src/app/sum.controller.js
+++ src/app/sum.controller.js
@@ -12,6 +12,9 @@
 		/* get from the url n1 and n2 numbers */
 		vm.firstNumber = $stateParams.n1;
 		vm.secondNumber = $stateParams.n2;
+		
+		vm.currencyList = fx.rates;
+		vm.selectedSymbol = "CAD";
 	}
 	
 })();
